dc50fd0f0eaef4eec906cf727256f46dbc8fdc79
iftekar2 <iftekar.hossain123@gmail.com>
2024-11-28 17:40:10 -0500

    Implementing Contract class

--- src/main/java/com/pluralsight/Contracts/Contract.java
+++ src/main/java/com/pluralsight/Contracts/Contract.java
@@ -1,4 +1,52 @@
 package com.pluralsight.Contracts;
 
-public class Contract {
+import com.pluralsight.Vehicle;
+
+public abstract class Contract {
+    private int dateOfContract;
+    private String customerName;
+    private String customerEmail;
+    private Vehicle vehicleSold;
+
+    public Contract(int dateOfContract, String customerName, String customerEmail, Vehicle vehicleSold) {
+        this.dateOfContract = dateOfContract;
+        this.customerName = customerName;
+        this.customerEmail = customerEmail;
+        this.vehicleSold = vehicleSold;
+    }
+
+    public int getDateOfContract() {
+        return dateOfContract;
+    }
+
+    public void setDateOfContract(int dateOfContract) {
+        this.dateOfContract = dateOfContract;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public String getCustomerEmail() {
+        return customerEmail;
+    }
+
+    public void setCustomerEmail(String customerEmail) {
+        this.customerEmail = customerEmail;
+    }
+
+    public Vehicle getVehicleSold() {
+        return vehicleSold;
+    }
+
+    public void setVehicleSold(Vehicle vehicleSold) {
+        this.vehicleSold = vehicleSold;
+    }
+
+    public abstract double getTotalPrice();
+    public abstract double getMonthlyPayment();
 }
